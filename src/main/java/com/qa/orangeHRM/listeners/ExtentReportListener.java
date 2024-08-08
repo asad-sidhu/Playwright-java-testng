9e852b67576a04c52b6b26a82e98515354e3ab6a
AsadSidhu <asad.sidhu@tenx.ai>
2024-08-09 01:53:23 +0500

    updated testNG.xml

--- src/main/java/com/qa/orangeHRM/listeners/ExtentReportListener.java
+++ src/main/java/com/qa/orangeHRM/listeners/ExtentReportListener.java
@@ -69,7 +69,8 @@ public class ExtentReportListener implements ITestListener, IClassListener {
         System.out.println("Test Class started: " + testClass.getName());
         String className = testClass.getName();
         String dateTime = DateUtil.getCurrentTime();
-        String generatedReportName = className.split("test.")[1] + "_" + dateTime + ".html";
+//        String generatedReportName = className.split("test.")[1] + "_" + dateTime + ".html";
+        String generatedReportName = "className.html";
         extentReports.set(init(generatedReportName));
         reportName.set(generatedReportName);
     }
